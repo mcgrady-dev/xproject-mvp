0cd963315d28664625ff47d581b487f58c19e581
mogui <mogui@weyee.com>
2017-10-17 17:52:53 +0800

    rxjava test code

--- app/src/main/java/com/mcgrady/xproject/ui/gold/fragment/GoldHomeFragment.java
+++ app/src/main/java/com/mcgrady/xproject/ui/gold/fragment/GoldHomeFragment.java
@@ -8,11 +8,17 @@ import com.mcgrady.xproject.R;
 import com.mcgrady.xproject.base.BaseFragment;
 import com.mcgrady.xproject.contract.GoldHomeContract;
 import com.mcgrady.xproject.presenter.GoldHomePresenter;
+import com.mcgrady.xproject.util.LogUtil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import butterknife.BindView;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by mcgrady on 2017/8/30.
@@ -39,11 +45,106 @@ public class GoldHomeFragment extends BaseFragment<GoldHomePresenter> implements
         mTableLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
         mTableLayout.setupWithViewPager(mViewPager);
 
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add("hello +" + i);
+        }
+
+        // 遍历
+//        Observable<String> observable = Observable.fromIterable(list);
+
+        // 每次订阅都会创建一个新的Observable，并且如果没有被订阅，就不会产生新的Observable
+//        SomeType instance = new SomeType();
+//        Observable<String> observable = instance.valueObservable();
+//        instance.setValue("hello");
+//
+//        Observer<String> observer = new Observer<String>() {
+//            @Override
+//            public void onSubscribe(@NonNull Disposable d) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(@NonNull String s) {
+//                LogUtil.d(s);
+//            }
+//
+//            @Override
+//            public void onError(@NonNull Throwable e) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        };
+//
+//        observable.subscribe(observer);
+
+        // 定时发送
+//        Observable.interval(1, TimeUnit.SECONDS)
+//                .take(15)
+//                .window(10, TimeUnit.SECONDS)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Consumer<Observable<Long>>() {
+//                    @Override
+//                    public void accept(@NonNull Observable<Long> longObservable) throws Exception {
+//                        LogUtil.d("doing...");
+//
+//                        longObservable.subscribeOn(Schedulers.io())
+//                                .observeOn(AndroidSchedulers.mainThread())
+//                                .subscribe(new Consumer<Long>() {
+//                                    @Override
+//                                    public void accept(@NonNull Long aLong) throws Exception {
+//                                        LogUtil.d("hello " + aLong);
+//                                    }
+//                                });
+//                    }
+//                });
+
+        Observable.range(1, 20)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        LogUtil.d("hello " + integer);
+                    }
+                });
+
+
     }
 
     @Override
     protected void initInject() {
         getFragmentComponent().inject(this);
     }
+
+    class SomeType {
+        private String value = "";
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public Observable<String> valueObservable() {
+//            return Observable.just(value);
+
+//            return Observable.create(new ObservableOnSubscribe<String>() {
+//                @Override
+//                public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+//                    e.onNext(value);
+//                }
+//            });
+
+            return Observable.defer(new Callable<ObservableSource<? extends String>>() {
+                @Override
+                public ObservableSource<? extends String> call() throws Exception {
+                    return Observable.just(value);
+                }
+            });
+        }
+    }
+
 
 }
