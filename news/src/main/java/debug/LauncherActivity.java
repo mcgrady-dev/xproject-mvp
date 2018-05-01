e5aeeebbd6fc87a93ce9c650ca326830a435311d
mcgrady911 <mcgrady911@foxmail.com>
2018-05-01 22:13:06 +0800

    update

---
+++ news/src/main/java/debug/LauncherActivity.java
@@ -0,0 +1,19 @@
+package debug;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author mcgrady
+ * @tieme 2018/4/29 23:43
+ * @des 组件开发模式下，用于传递数据的启动Activity，集成模式下无效
+ */
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
