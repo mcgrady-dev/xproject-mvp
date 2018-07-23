c421046e34a6be55f96e4e6784e6a65018a8188c
mcgrady <mogui@weyee.com>
2018-07-23 18:12:03 +0800

    add module name

--- common-core/src/main/java/com/mcgrady/core/Constants.java
+++ common-core/src/main/java/com/mcgrady/core/Constants.java
@@ -12,4 +12,7 @@ public class Constants {
     public static final String VERSION = "version";
 
     public static final String SP_FILE_NAME_VERSION = "version_file_name";
+
+    public static final String MODULE_NEWS_NAME = "/news";
+    public static final String MODULE_MAIN_NAME = "/main";
 }
