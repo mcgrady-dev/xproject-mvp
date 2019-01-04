881d9b38e8e3b998b6491645cdd41430766f148d
mcgrady <mogui@weyee.com>
2019-01-04 14:33:51 +0800

    Update zhihu daily home banner

--- module_main/src/main/java/com/mcgrady/main/mvp/ui/activity/ZhihuDailySplashActivity.java
+++ module_main/src/main/java/com/mcgrady/main/mvp/ui/activity/ZhihuDailySplashActivity.java
@@ -3,12 +3,20 @@ package com.mcgrady.main.mvp.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.mcgrady.common_core.base.BaseActivity;
+import com.mcgrady.common_core.config.RouterHub;
 import com.mcgrady.common_core.di.component.AppComponent;
 import com.mcgrady.main.R;
+import com.mcgrady.main.R2;
+
+import butterknife.BindView;
 
 /**
  * <p>类说明</p>
@@ -16,8 +24,11 @@ import com.mcgrady.main.R;
  * @author: mcgrady
  * @date: 2019/3/4
  */
-
+@Route(path = RouterHub.ZHIHU_DAILY_SPLASH)
 public class ZhihuDailySplashActivity extends BaseActivity {
+
+    @BindView(R2.id.main_iv_splash)
+    ImageView ivSplash;
 
     private Animation splashAnimation;
 
@@ -35,5 +46,31 @@ public class ZhihuDailySplashActivity extends BaseActivity {
     public void initData(@Nullable Bundle savedInstanceState) {
         splashAnimation = AnimationUtils.loadAnimation(this, R.anim.public_splash);
 
+        splashAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                ivSplash.setVisibility(View.GONE);
+                ARouter.getInstance().build(RouterHub.ZHIHU_DAILY_HOME)
+                        .navigation(ZhihuDailySplashActivity.this);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        ivSplash.post(new Runnable() {
+            @Override
+            public void run() {
+                ivSplash.startAnimation(splashAnimation);
+            }
+        });
     }
 }
