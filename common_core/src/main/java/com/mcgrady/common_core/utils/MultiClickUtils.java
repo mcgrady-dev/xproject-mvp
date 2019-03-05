c0590b40bd6ac24f334350a9249427d204523d51
mcgrady <mogui@weyee.com>
2019-03-05 17:36:32 +0800

    Add utils

---
+++ common_core/src/main/java/com/mcgrady/common_core/utils/MultiClickUtils.java
@@ -0,0 +1,35 @@
+package com.mcgrady.core.utils;
+
+/**
+ * <p>检查View重复点击工具</p>
+ *
+ * @author: mcgrady
+ * @date: 2018/9/3
+ */
+
+public class MultiClickUtil {
+
+    private static final long CLICK_INTERVAL = 500L;
+    private static long lastClickTime = 0L;
+
+    /**
+     * 重置点击过滤器, 在每次用代码模拟View点击事件的时候, 要先reset一下, 以防被过滤掉
+     */
+    public static void resetMultiClick() {
+        lastClickTime = 0L;
+    }
+
+    public static boolean isMultiClick() {
+        return isMultiClick(CLICK_INTERVAL);
+    }
+
+    public static boolean isMultiClick(long limitTime) {
+        long time = System.currentTimeMillis();
+        if ((time - lastClickTime) > limitTime) {
+            lastClickTime = time;
+            return false;
+        }
+        lastClickTime = time;
+        return true;
+    }
+}
