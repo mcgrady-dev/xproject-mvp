31b4b33da951be860c9ebaae1715a6a63afd90d7
mcgrady911 <mcgrady911@foxmail.com>
2019-04-19 23:48:15 +0800

    Add module test

---
+++ module_test/src/main/java/com/mcgrady/test/TestActivity.java
@@ -0,0 +1,18 @@
+package com.mcgrady.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.blankj.utilcode.util.LogUtils;
+
+public class TestActivity extends AppCompatActivity {
+
+    public static final String TAG = TestActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LogUtils.d(TAG, "onCreate");
+        setContentView(R.layout.test_activity_test);
+    }
+}
