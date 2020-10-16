095c84fbbd1539b2907fb01fb3349ea533edba63
mcgrady <mcgrady911@foxmail.com>
2020-10-16 09:58:39 +0800

    test agent web view

---
+++ module-test/src/main/java/com/mcgrady/module_test/activity/WebViewActivity.java
@@ -0,0 +1,119 @@
+package com.mcgrady.module_test.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.blankj.utilcode.util.ProcessUtils;
+import com.mcgrady.module_test.R;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webView = findViewById(R.id.webview);
+
+        WebSettings mWebSettings = webView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setSupportZoom(true);
+        mWebSettings.setBuiltInZoomControls(false);
+        mWebSettings.setSavePassword(false);
+//        if (AgentWebUtils.checkNetwork(webView.getContext())) {
+            //根据cache-control获取数据。
+            mWebSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+//        } else {
+//            //没网，则从本地获取，即离线加载
+//            mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //适配5.0不允许http和https混合使用情况
+            mWebSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+        mWebSettings.setTextZoom(100);
+        mWebSettings.setDatabaseEnabled(true);
+        mWebSettings.setAppCacheEnabled(true);
+        mWebSettings.setLoadsImagesAutomatically(true);
+        mWebSettings.setSupportMultipleWindows(false);
+        // 是否阻塞加载网络图片  协议http or https
+        mWebSettings.setBlockNetworkImage(false);
+        // 允许加载本地文件html  file协议
+        mWebSettings.setAllowFileAccess(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            // 通过 file url 加载的 Javascript 读取其他的本地文件 .建议关闭
+            mWebSettings.setAllowFileAccessFromFileURLs(false);
+            // 允许通过 file url 加载的 Javascript 可以访问其他的源，包括其他的文件和 http，https 等其他的源
+            mWebSettings.setAllowUniversalAccessFromFileURLs(false);
+        }
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+
+            mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        } else {
+            mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+        }
+        mWebSettings.setLoadWithOverviewMode(false);
+        mWebSettings.setUseWideViewPort(false);
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setNeedInitialFocus(true);
+        mWebSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+        mWebSettings.setDefaultFontSize(16);
+        mWebSettings.setMinimumFontSize(12);//设置 WebView 支持的最小字体大小，默认为 8
+        mWebSettings.setGeolocationEnabled(true);
+//        String dir = AgentWebConfig.getCachePath(webView.getContext());
+//        LogUtils.i(TAG, "dir:" + dir + "   appcache:" + AgentWebConfig.getCachePath(webView.getContext()));
+//        //设置数据库路径  api19 已经废弃,这里只针对 webkit 起作用
+//        mWebSettings.setGeolocationDatabasePath(dir);
+//        mWebSettings.setDatabasePath(dir);
+//        mWebSettings.setAppCachePath(dir);
+        //缓存文件最大值
+        mWebSettings.setAppCacheMaxSize(Long.MAX_VALUE);
+//        mWebSettings.setUserAgentString(mWebSettings
+//                .getUserAgentString()
+//                .concat(USERAGENT_AGENTWEB)
+//                .concat(USERAGENT_UC)
+//        );
+//        LogUtils.i(TAG, "UserAgentString : " + mWebSettings.getUserAgentString());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // 安卓9.0后不允许多进程使用同一个数据目录，需设置前缀来区分
+            // 参阅 https://blog.csdn.net/lvshuchangyin/article/details/89446629
+            String processName = ProcessUtils.getCurrentProcessName();
+            if (!getApplicationContext().getPackageName().equals(processName)) {
+                WebView.setDataDirectorySuffix(processName);
+            }
+        }
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+//                //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
+//                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                    view.loadUrl(request.getUrl().toString());
+//                    return true;
+//                }
+//
+//                return false;
+            }
+        });
+
+        webView.loadUrl("https://finegw.cx580.com/merauthorization?user_id=413881&user_from=bluefocus2017&carnumber=粤AQ538M&token=QbkQ35SvMv0j2H0OfHwX%2Fo0sJvVZ49b9k%2FBlLzxVYZM%3D");
+    }
+}
