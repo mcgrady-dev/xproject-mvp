80f0a3268304420202b6fea04b05652ade5ba63c
mcgrady <mcgrady911@foxmail.com>
2020-11-26 10:53:57 +0800

    add main module global config

---
+++ module-main/src/main/java/com/mcgrady/main/app/GlobalConfig.java
@@ -0,0 +1,36 @@
+package com.mcgrady.main.mvp.app;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+
+import com.mcgrady.xskeleton.base.AppLifecycles;
+import com.mcgrady.xskeleton.integration.ConfigModule;
+import com.mcgrady.xskeleton.module.GlobalConfigModule;
+
+import java.util.List;
+
+public class GlobalConfig implements ConfigModule {
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlobalConfigModule.Builder builder) {
+
+    }
+
+    @Override
+    public void injectAppLifecycle(@NonNull Context context, @NonNull List<AppLifecycles> lifecycles) {
+
+    }
+
+    @Override
+    public void injectActivityLifecycle(@NonNull Context context, @NonNull List<Application.ActivityLifecycleCallbacks> lifecycles) {
+
+    }
+
+    @Override
+    public void injectFragmentLifecycle(@NonNull Context context, @NonNull List<FragmentManager.FragmentLifecycleCallbacks> lifecycles) {
+
+    }
+}
