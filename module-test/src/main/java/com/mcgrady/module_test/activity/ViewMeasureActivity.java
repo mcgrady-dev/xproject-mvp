9772388e59a7775f834929675ab0a5182996d781
mcgrady <mcgrady911@foxmail.com>
2021-03-22 15:04:22 +0800

    add view measure demo

---
+++ module-test/src/main/java/com/mcgrady/module_test/activity/ViewMeasureActivity.java
@@ -0,0 +1,16 @@
+package com.mcgrady.module_test.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mcgrady.module_test.R;
+
+public class ViewMeasureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_measure);
+    }
+}
