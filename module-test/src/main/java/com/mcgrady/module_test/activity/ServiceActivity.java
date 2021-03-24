3eb86e886d6f48938902c2cb176927c80de90130
mcgrady <mcgrady911@foxmail.com>
2021-03-24 14:22:27 +0800

    add test demo

---
+++ module-test/src/main/java/com/mcgrady/module_test/activity/ServiceActivity.java
@@ -0,0 +1,14 @@
+package com.mcgrady.module_test.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+    }
+}
